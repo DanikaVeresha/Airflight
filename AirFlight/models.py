509eb7756cd4de36b22a193cc6b65b97354f5b5c
daria.ch.prozorro <daria.ch.prozorro@gmail.com>
2023-03-14 14:09:57 +0200

    Airlines

--- AirFlight/models.py
+++ AirFlight/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-class AirFlight(models.Model):
-    Flight_id = models.UUIDField()
-    Pilot = models.ForeignKey('Pilot', on_delete=models.CASCADE)
-    Exception = models.ForeignKey('ExceptionFlight', on_delete=models.CASCADE)
-    Status_flight = models.CharField(default='straight', max_length=20)
-    Flight_status_by_passengers = models.CharField(default='empty', max_length=20)
-    Flight_type = models.CharField(default='passenger', max_length=20)
-    Flight_price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
-    Flight_date = models.DateField(null=True)
-
-
 class Pilot(models.Model):
     Pilot_name = models.CharField(max_length=100)
 
@@ -22,29 +10,40 @@ class ExceptionFlight(models.Model):
     Exception_description = models.CharField(max_length=100)
 
 
-class Flight(models.Model):
+# Create your models here.
+class AirFlight(models.Model):
     Flight_id = models.UUIDField()
-    StartingPoint = models.ForeignKey('StartingPoint', on_delete=models.CASCADE)
-    PointOfArrival = models.ForeignKey('PointOfArrival', on_delete=models.CASCADE)
-    ExceptionPoint = models.ForeignKey('ExceptionPoint', on_delete=models.CASCADE)
+    Pilot = models.ForeignKey(Pilot.Pilot_name, on_delete=models.CASCADE)
+    Exception = models.ForeignKey(ExceptionFlight.Exception_name, on_delete=models.CASCADE)
+    Status_flight = models.CharField(default='straight', max_length=20)
+    Flight_status_by_passengers = models.CharField(default='empty', max_length=20)
+    Flight_type = models.CharField(default='passenger', max_length=20)
+    Flight_price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
+    Flight_date = models.DateField(null=True)
 
 
 class StartingPoint(models.Model):
-    Flight_id = models.UUIDField()
     StartingPoint_name = models.CharField(max_length=100)
     Airport_latitude_coordinate = models.DecimalField(decimal_places=2, max_digits=10, null=True)
     Airport_longitude_coordinate = models.DecimalField(decimal_places=2, max_digits=10, null=True)
 
 
 class PointOfArrival(models.Model):
-    Flight_id = models.UUIDField()
     PointOfArrival_name = models.CharField(max_length=100)
     Airport_latitude_coordinate = models.DecimalField(decimal_places=2, max_digits=10, null=True)
     Airport_longitude_coordinate = models.DecimalField(decimal_places=2, max_digits=10, null=True)
 
 
 class ExceptionPoint(models.Model):
-    Flight_id = models.UUIDField()
     ExceptionPoint_name = models.CharField(max_length=100)
     Airport_latitude_coordinate = models.DecimalField(decimal_places=2, max_digits=10, null=True)
     Airport_longitude_coordinate = models.DecimalField(decimal_places=2, max_digits=10, null=True)
+
+
+class Flight(models.Model):
+    Flight_id = models.UUIDField()
+    StartingPoint = models.ForeignKey(StartingPoint.StartingPoint_name, on_delete=models.CASCADE)
+    PointOfArrival = models.ForeignKey(PointOfArrival.PointOfArrival_name, on_delete=models.CASCADE)
+    ExceptionPoint = models.ForeignKey(ExceptionPoint.ExceptionPoint_name, on_delete=models.CASCADE)
+
+
